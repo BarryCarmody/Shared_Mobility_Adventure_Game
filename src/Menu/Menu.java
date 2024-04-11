fef7731f300bc5f2bfda6aeb4ee2c14c3c521d86
rizalynne22 <rizalynne@hotmail.com>
2024-04-11 01:09:27 +0100

    First commit of menu

---
+++ src/Menu/Menu.java
@@ -0,0 +1,15 @@
+package Menu;
+
+public class Menu {
+    static int width = 800;
+    static int height = 600;
+    static String title = "EcoRiders";
+
+    public static void main(String[] args) {
+        Window window = new Window(width, height, title);
+
+        Thread thread = new Thread(window);
+        thread.start();
+
+    }
+}
